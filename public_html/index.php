746d096f002f88b78220860579b9dd500ff2f80c
Keita Momotani <keita.momotani@gmail.com>
2017-11-12 16:13:37 +0900

    create login

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,15 @@
 <?php
-echo 'aaa';
+const LOGIN_ID = 'login';
+const LOGIN_PASSWORD = 'password';
+
+$login_id = '';
+$login_password = '';
+
+$login_id = isset($_POST['login_id']) ? $_POST['login_id'] : '';
+$login_password = isset($_POST['login_password']) ? $_POST['login_password'] : '';
+
+if (LOGIN_ID != $login_id && LOGIN_PASSWORD != $login_password ) {
+    header('Location: login.php');
+    }
+
+echo 'ログインしました';
---
+++ public_html/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <form action='index.php' method='post'>
+        <table border=2>
+            <tr>
+                <th>login id</th>
+                <th>password</th>
+            </tr>
+            <tr>
+                <td><input type='text' name='login_id'></td>
+                <td><input type='text' name='login_password'></td>
+            </tr>
+        </table>
+        <input type='submit' value='ログイン'>
+    </form>
+
+</body>
+</html>
